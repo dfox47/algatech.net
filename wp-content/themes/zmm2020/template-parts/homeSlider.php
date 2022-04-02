060eddb666564f64503ce9d0ae433b0b30c4b810
Dennis FOX <dfox@foxartbox.com>
2022-04-02 12:54:24 +0300

    first commit

---
+++ wp-content/themes/zmm2020/template-parts/homeSlider.php
@@ -0,0 +1,31 @@
+
+
+
+<div class="home_slider">
+	<div class="home_slider__wrap">
+		<div class="owl-carousel">
+<!--			<a class="home_slider_item" href="/product/abriht-shhrajhmus-sicar-300-c-2">-->
+<!--				<span class="home_slider_img home_slider_img_2"></span>-->
+<!---->
+<!--				<span class="home_slider_desc">-->
+<!--					<span class="home_slider_title">Форматно разкройваща маса и подрезач</span>-->
+<!---->
+<!--					<span class="home_slider_content">-->
+<!--						SICAR C300 - <span class="home_slider_special">от</span> <span class="bold">10450.00 лв</span><br />-->
+<!--					</span>-->
+<!---->
+<!--					<img class="home_slider_raif" src="--><?php //echo $iconsDir; ?><!--/raif.svg" alt="" />-->
+<!--					<img class="home_slider_raif" src="--><?php //echo $iconsDir; ?><!--/unicredit.svg" alt="" />-->
+<!---->
+<!--					<span class="btn btn_main home_slider_btn">Напред</span>-->
+<!--				</span>-->
+<!--			</a>-->
+
+			<a class="home_slider_item" href="">
+				<img src="/wp-content/themes/zmm2020/images/home_slider/5.jpg" alt="" />
+			</a>
+		</div>
+	</div>
+</div>
+
+
---
+++ wp-content/themes/zmm2020/template-parts/productsLinks.php
@@ -0,0 +1,30 @@
+
+
+
+<h2>Продукти</h2>
+
+<div class="home_products_links__wrap">
+	<div class="home_products_links">
+		<a class="home_products_links__item" href="/product-category/formatno-razkrojvashhi-czirkulyari/">
+			<span class="home_products_links__item__img"></span>
+			<span class="home_products_links__item__desc">Форматно разкройващи циркуляри</span>
+		</a>
+
+		<a class="home_products_links__item" href="/product-category/kantoslepvashhi-mashini-2/">
+			<span class="home_products_links__item__img"></span>
+			<span class="home_products_links__item__desc">Кантослепващи машини</span>
+		</a>
+
+		<a class="home_products_links__item" href="/product-category/cnc-obrabotvashhi-czentri/">
+			<span class="home_products_links__item__img"></span>
+			<span class="home_products_links__item__desc">CNC обработващи центри</span>
+		</a>
+
+		<a class="home_products_links__item" href="/product-category/paketo-razkrojvashhi-czirkulyari-czentri/">
+			<span class="home_products_links__item__img"></span>
+			<span class="home_products_links__item__desc">Пакето разкройващи циркуляри (центри)</span>
+		</a>
+	</div>
+</div>
+
+
